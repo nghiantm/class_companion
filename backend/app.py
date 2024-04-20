90b6ee13c9c9abf1a9949801f8f42fcb67222b7c
Pham Callista <phamcallista@air-cua-pham.mynetworksettings.com>
2024-04-20 18:28:38 -0400

    Added generate response route

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,23 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from together import Together
+from pydantic import BaseModel, Field
+from typing import List
+#from together import Together
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 # Together.ai
-together_client = Together(api_key=os.environ.get("TOGETHER_KEY"))
+together_client = OpenAI(api_key=os.getenv("TOGETHER_API_KEY"), base_url=os.getenv("BASE_URL"))
+# Define the schema for the output.
+class User(BaseModel):
+    questions: List[str] = Field(
+        description="List of questions from 1 to 6"
+    )
+
+#client = OpenAI(api_key=os.getenv("TOGETHER_API_KEY"), base_url=os.getenv("BASE_URL"))
 # Mongo
 mongo_client = MongoClient(os.environ.get("MONGO_URL"))
 db = mongo_client.ClassCompanion
@@ -56,6 +65,29 @@ def post_session():
         sessions_collection.update_one(query, update_operation, upsert=True)
 
         return jsonify(data), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route("/generate_response", methods=["POST"])
+def generate_response():
+    try:
+        data = request.get_json()
+        messages = data["messages"]
+
+        response = together_client.chat.completions.create(
+            messages=messages,
+            #response_format={"type": "json_object", "schema": User.model_json_schema()},
+            temperature=1,
+            model="NousResearch/Nous-Hermes-2-Yi-34B"
+            #model="mistralai/Mixtral-8x7B-v0.1"
+        )
+
+        responseJSON = {
+            "role": response.choices[0].message.role,
+            "content": response.choices[0].message.content
+        }
+
+        return jsonify(responseJSON), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
